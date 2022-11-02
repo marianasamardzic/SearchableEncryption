8029c18123e58f906b1fe42b1cbff99031217c44
Andrei Geadau <andrei.geadau@yahoo.com>
2022-11-02 23:23:01 +0100

    added failing test!

--- test_main.py
+++ test_main.py
@@ -54,3 +54,26 @@ class Test(TestCase):
             decrypted_outputs.append(intListToStr(self.client1.decryptFile(output, self.client1.sk)))
 
         assert len(outputs) == 0
+
+    def test_multiple_queries_not_working(self):
+        self.consultant.store_to_server("First document", [self.consultant.pk, self.client0.pk],
+                                        ['id12', '2017', 'jan', 'report', 'None'])
+        self.consultant.store_to_server("Second document", [self.consultant.pk, self.client0.pk],
+                                        ['id12', '2017', 'jan', 'report', 'None'])
+
+        ## RUN A QUERY FOR THE FIRST TIME
+        trap = Trapdoor([1], ['2017'], self.client0.t, self.client0.sk)
+
+        outputs = self.server.test_on_all_docs(self.client0.pk, trap)
+        decrypted_outputs = []
+        for output in outputs:
+            decrypted_outputs.append(intListToStr(self.client0.decryptFile(output, self.client0.sk)))
+        assert len(outputs) == 2
+
+        # RUN THE SAME QUERY AGAIN -- not working!!!
+        another_trap = Trapdoor([1], ['2017'], self.client0.t, self.client0.sk)
+        another_outputs = self.server.test_on_all_docs(self.client0.pk, another_trap)
+        another_decrypted_outputs = []
+        for another_output in another_outputs:
+            another_decrypted_outputs.append(intListToStr(self.client0.decryptFile(another_output, self.client0.sk)))
+        assert len(another_outputs) == 2  # but is 0
