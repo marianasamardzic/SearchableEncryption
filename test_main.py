1fe90ad983b002c54c8723ef27a1ddeae5ce07d7
Andrei Geadau <andrei.geadau@yahoo.com>
2022-11-11 06:45:29 +0000

    Improve tests

--- test_main.py
+++ test_main.py
@@ -4,11 +4,13 @@ from main import Server, Sender, Trapdoor, intListToStr
 
 
 class Test(TestCase):
-    server = Server()
-    consultant = Sender(server)
-    client0 = Sender(server)
-    client1 = Sender(server)
-    mal = Sender(server)
+
+    def setUp(self):
+        self.server = Server()
+        self.consultant = Sender(self.server)
+        self.client0 = Sender(self.server)
+        self.client1 = Sender(self.server)
+        self.mal = Sender(self.server)
 
     def test_consultantCanReadItsOwnData(self):
         self.consultant.store_to_server("First document", [self.consultant.pk, self.client0.pk],
@@ -55,7 +57,7 @@ class Test(TestCase):
 
         assert len(outputs) == 0
 
-    def test_multiple_queries_not_working(self):
+    def test_multiple_queries_give_consistent_results(self):
         self.consultant.store_to_server("First document", [self.consultant.pk, self.client0.pk],
                                         ['id12', '2017', 'jan', 'report', 'None'])
         self.consultant.store_to_server("Second document", [self.consultant.pk, self.client0.pk],
@@ -63,17 +65,29 @@ class Test(TestCase):
 
         ## RUN A QUERY FOR THE FIRST TIME
         trap = Trapdoor([1], ['2017'], self.client0.t, self.client0.sk)
-
         outputs = self.server.test_on_all_docs(self.client0.pk, trap)
         decrypted_outputs = []
         for output in outputs:
             decrypted_outputs.append(intListToStr(self.client0.decryptFile(output, self.client0.sk)))
-        assert len(outputs) == 2 # it fails here as well
+        assert len(outputs) == 2
 
-        # RUN THE SAME QUERY AGAIN -- not working!!!
+        # RUN THE SAME QUERY AGAIN
         another_trap = Trapdoor([1], ['2017'], self.client0.t, self.client0.sk)
         another_outputs = self.server.test_on_all_docs(self.client0.pk, another_trap)
         another_decrypted_outputs = []
         for another_output in another_outputs:
             another_decrypted_outputs.append(intListToStr(self.client0.decryptFile(another_output, self.client0.sk)))
-        assert len(another_outputs) == 2  # but is 0
+        assert len(another_outputs) == 2
+
+    def test_malicious_clinet_can_insert_data_for_others(self):
+        self.mal.store_to_server("First document", [self.consultant.pk, self.client0.pk],
+                                        ['id12', '2017', 'jan', 'report', 'None'])
+
+        trap = Trapdoor([1], ['2017'], self.client0.t, self.client0.sk)
+
+        outputs = self.server.test_on_all_docs(self.client0.pk, trap)
+        decrypted_outputs = []
+        for output in outputs:
+            decrypted_outputs.append(intListToStr(self.client0.decryptFile(output, self.client0.sk)))
+
+        assert len(outputs) == 1
